2eeda31be7f1c903e3524e13e27fa2247eb6b066
emmanuel971-source <emmanuel.pierre@yahoo.fr>
2026-03-25 22:15:44 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'kanadrop-v1';
+const ASSETS = [
+  '/fck/',
+  '/fck/index.html',
+  '/fck/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Network first, cache fallback
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
