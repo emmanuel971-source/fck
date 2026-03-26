bd2eab22daeb9617076910a495aa784a45f6e2e7
emmanuel971-source <emmanuel.pierre@yahoo.fr>
2026-03-25 21:22:31 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,30 @@
-const CACHE = 'kanadrop-v1';
+const CACHE_NAME = 'kanadrop-v99';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json' // Ajoute le manifeste ici, Android apprécie
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
-self.addEventListener('install', e => {
+
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => { if (key !== CACHE_NAME) return caches.delete(key); })
+    ))
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
